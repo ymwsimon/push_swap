0cb347a5afd176575caff0f25f7a358d00d25ea3
Simon <mayeung@student.42london.com>
2024-01-17 22:56:17 +0000

    qsort

--- srcs/quicksort.c
+++ srcs/quicksort.c
@@ -6,7 +6,7 @@
 /*   By: mayeung <mayeung@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 15:36:39 by mayeung           #+#    #+#             */
-/*   Updated: 2024/01/17 22:39:10 by mayeung          ###   ########.fr       */
+/*   Updated: 2024/01/17 22:53:05 by mayeung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,20 +29,22 @@ void    quicksort(t_stac *sts, int low, int high, char from)
 	int	med;
 	int	i;
 	int	rotate;
+	int	push;
 
 	med = (low + high + 1) / 2;
 	i = low;
 	rotate = 0;
+	push = 0;
 	if (low > high || (low == high && from == 'a'))
 		return ;
-	while (i <= high && (high - low > 1 || from == 'b'))
+	while (i <= high && push <= (high - low) / 2 && (high - low > 1 || from == 'b'))
 	{
 		if (from == 'a' && sts->ra[0] < med)
-			ft_push(NO_UPDATE_RANK, sts, PRINT, "pb\n");
+			ft_push(NO_UPDATE_RANK, sts, PRINT * ++push, "pb\n");
 		else if (from == 'a' && sts->ra[0] >= med)
 			ft_rotate(sts, -1, PRINT * ++rotate, "ra\n");
 		else if (from == 'b' && sts->rb[0] >= med)
-			ft_push(NO_UPDATE_RANK, sts, PRINT, "pa\n");
+			ft_push(NO_UPDATE_RANK, sts, PRINT * ++push, "pa\n");
 		else if (from == 'b' && sts->rb[0] < med)
 			ft_rotate(sts, -1, PRINT * ++rotate, "rb\n");
 		i++;
